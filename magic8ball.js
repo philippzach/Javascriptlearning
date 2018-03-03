a5baac02b7fbbe7213f3e168c3fcdea17532da73
philippzach <mail@philippzach.com>
2018-03-02 17:00:31 -1000

    Control Flow- Magic 8-Ball
    The user will be able to input a question, then our program will output a random fortune.

---
+++ magic8ball.js
@@ -0,0 +1,35 @@
+let userName = "";
+userName ? console.log(`Hello, ${userName}!`) :
+console.log("Hello!");
+let userQuestion = "Whats your Question?";
+let randomNumber = Math.floor(Math.random() * 8 - 0);
+let eightBall = "";
+eightBall = randomNumber;
+switch (eightBall) {
+  case 0:
+    eightBall = "It is certain";
+    break;
+    case 1:
+    eightBall = "It is decidedly so";
+    break;
+    case 2:
+    eightBall = "Reply hazy try again";
+    break;
+    case 3:
+    eightBall = "Cannot predict now";
+    break;
+    case 4:
+    eightBall = "Don't count on it";
+    break;
+    case 5:
+    eightBall = "My sources say no";
+    break;
+    case 6:
+    eightBall = "Outlook not so good";
+    break;
+    case 7:
+    eightBall = "Signs point to yes";
+    break;
+                 }
+console.log(userQuestion);
+console.log(eightBall);
